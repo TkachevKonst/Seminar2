3428f2c6ad271b19cc62e7e59d21336e136e3c75
Tkachev Konstantin <t_k_a90@mail.ru>
2023-06-18 19:25:54 +0600

    Д.з 2

---
+++ Задача13/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите любое число:");
+int num1 = int.Parse(Console.ReadLine());
+if (num1<99)
+{
+    Console.WriteLine("двухзначное число");
+    return;
+}
+while (num1>1000)
+{
+   num1/=10;
+ }
+    Console.WriteLine(num1);
+    Console.WriteLine(num1%10);
---
+++ Задача15/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите любое число:");
+int num1 = int.Parse(Console.ReadLine());
+if (num1 ==6 || num1==7)
+{
+    Console.WriteLine("Выходной");
+}
+else if (num1 > 0 && num1 < 6)
+{
+    Console.WriteLine("Будний");
+}
+else
+{
+    Console.WriteLine("Ошибка");
+}
